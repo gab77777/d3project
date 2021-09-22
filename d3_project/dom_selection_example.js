7bfbc3fdc7a123a4eec43389018147b30fdd9ca4
Gabriel Poma <gabrielpoma96@MacBook-Pro-de-apple.local>
2021-09-22 16:54:04 -0500

    d3_project graphs

---
+++ d3_project/dom_selection_example.js
@@ -0,0 +1,6 @@
+function main() {
+        //d3 code goes here, # for specific id
+        //d3.selectAll(".myclass").style("color", "navy")
+        d3.select("tr").selectAll("td").style("background-color", "firebrick");
+
+} 
