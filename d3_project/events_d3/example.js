f23727fd03b16955ea8895a90e828115cc752981
Gabriel Poma <gabrielpoma96@MacBook-Pro-de-apple.local>
2021-09-23 16:54:36 -0500

    Box Mouseover-out

---
+++ d3_project/events_d3/example.js
@@ -0,0 +1,13 @@
+function main() {
+        //d3 code goes here
+        d3.selectAll("div")
+                .on("mouseover", function(){
+                        d3.select(this)
+                                .style("background-color","firebrick")
+                })
+                .on("mouseout", function() {
+                        d3.select(this)
+                                .style("background-color", "navy")
+                })
+
+} 
